79090e3be3cb51fea57a5b4aeb039e97f8da8203
harshil93 <lodhi.harshil@gmail.com>
2016-05-16 00:30:02 +0530

    Sampled extension cloned and the UI contribution point modified

---
+++ scripts/app.ts
@@ -0,0 +1,21 @@
+﻿/// <reference path='../typings/tsd.d.ts' />
+
+var actionProvider1 = {
+    execute: (context) => {
+        alert("hello world");
+    }
+}
+
+// Register context menu action provider
+var showVariablesContext = (function () {
+    "use strict";
+
+    return {
+        execute: function (sourceItemContext) {
+            // Load REST client
+                alert("I am loaded. hurray");
+     }
+	} 
+}());
+
+VSS.register("show-variables", showVariablesContext);
